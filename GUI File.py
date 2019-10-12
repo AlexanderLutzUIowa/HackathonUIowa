4eeae58c0be8fbeb6eab9da8d368d2584ce1552a
AlexanderLutzUIowa <alexander-lutz@uiowa.edu>
2019-10-12 11:07:41 -0500

    gui commit

--- GUI File.py
+++ GUI File.py
@@ -12,6 +12,22 @@ def createGUI():
     canvas = Canvas(canvasAndGUI, height=200, width=200, relief=SUNKEN, borderwidth=2)
     canvas.pack(side=LEFT)
 
+    guiFrame = Frame(canvasAndGUI)
+
+    ballsFrame = Frame(guiFrame)
+    takeLabel = Label(ballsFrame, text='Take')
+    numBallsEntry = Entry(ballsFrame)
+    ballsLabel = Label(ballsFrame, text='balls')
+    takeLabel.pack(side=LEFT)
+    numBallsEntry.pack(side=LEFT)
+    ballsLabel.pack()
+
+    heapFrame = Frame(guiFrame)
+    heapLabel = Label(heapFrame, text='from heap')
+    whichHeapEntry = Entry(heapFrame)
+    heapLabel.pack(side=LEFT)
+    whichHeapEntry.pack()
+
 
 
 
