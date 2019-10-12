287d908c8086f0ef8f8a1e1d3b8d49ec2d51f40b
ethandeguzman <ethandg23@outlook.com>
2019-10-12 14:23:24 -0500

    added word1 which strips "?!'()-:"

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def mapMachine():
     urlBase = "https://api.nytimes.com/svc/archive/v1/2016/1.json?api-key=FLPiXUAqjSQsAzY0gBc2YvXYIGwPWbEy"
     stringResultFromNYT = urlopen(urlBase).read().decode('utf8')
     jsonResult = json.loads(stringResultFromNYT)
+    with open("data_file.json", "w") as write_file:
+        json.dump(jsonResult, write_file)
     resultSize = jsonResult["response"]["meta"]["hits"]
     i = 0
     titles = []
@@ -18,17 +20,23 @@ def mapMachine():
         print(i)
         if ("print_headline") in jsonResult["response"]["docs"][i]["headline"]:
             titles.append(jsonResult["response"]["docs"][i]["headline"]["print_headline"])
-        i = i + 1
+        i = i+1
+    print(i)
+
+
+    print("result size is ", resultSize)
 
     for title in titles:
         if title not in ["None", " ", None]:
             for word in title.split():
                 word = word.lower()
-                if word not in words_to_ignore:
-                    if word in words:
-                        words[word] = words[word] + 1
+                word1 = word.strip("?!'()-:")
+                if word1 not in words_to_ignore:
+                    if word1 in words:
+                        words[word1] = words[word1] + 1
                     else:
-                        words[word] = 1
+                        words[word1] = 1
+
     sortedWords = sorted(words.items(), key=lambda x: x[1], reverse=True)
     print(sortedWords)
     print(sortedWords[0])
