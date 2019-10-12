27969f7a24d98182e895c0019a1f35a1c7e883ce
AlexanderLutzUIowa <alexander-lutz@uiowa.edu>
2019-10-12 11:01:38 -0500

    gui commit

--- GUI File.py
+++ GUI File.py
@@ -1,4 +1,4 @@
-from tkinter import Tk, Button, Label, Entry, LEFT, RIGHT
+from tkinter import Tk, Canvas, Frame, Button, Label, Entry, END, LEFT, RIGHT, SUNKEN
 
 def createGUI():
     global rootWindow
@@ -11,4 +11,11 @@ def createGUI():
     canvasAndGUI = Frame(rootWindow)
     canvas = Canvas(canvasAndGUI, height=200, width=200, relief=SUNKEN, borderwidth=2)
     canvas.pack(side=LEFT)
+
+
+
+
+
+
+
     rootWindow.mainloop()
