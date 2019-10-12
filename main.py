332be7fe2159db01248a8dd0035d29eb2547dd86
kianw <kianweimer@gmail.com>
2019-10-12 11:06:01 -0500

    Init

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 
 from urllib.request import urlopen, urlretrieve
-
+import json
 
 
 def mapMachine():
     urlBase = "https://api.nytimes.com/svc/archive/v1/2019/1.json?api-key=FLPiXUAqjSQsAzY0gBc2YvXYIGwPWbEy"
-    stringResultFromGoogle = urlopen(urlBase).read().decode('utf8')
+    stringResultFromNYT = urlopen(urlBase).read().decode('utf8')
+    jsonResult = json.loads(stringResultFromNYT)
+    print(jsonResult)
 
 mapMachine()
